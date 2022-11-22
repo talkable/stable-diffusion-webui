df5143ef69c9b1156a4b85ae6a923344c09c7585
Oleksandr Bondar <aleksander.bondar@talkable.com>
2022-11-22 17:38:46 +0200

    enable insecure extensions

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 cd /home/ubuntu/stable-diffusion-webui
 git fetch
 git reset --hard origin/master
-screen -dmS main -t tab0 /home/ubuntu/stable-diffusion-webui/webui.sh --listen
+screen -dmS main -t tab0 /home/ubuntu/stable-diffusion-webui/webui.sh --listen --enable-insecure-extension-access
 screen -S main -X screen -t tab1 /home/ubuntu/stable-diffusion-webui/gdrive_sync.sh
