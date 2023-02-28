147cd84af350e0f36d7508f5e4cb345f6a491a87
sergii.kostiuk <sergii.kostiuk@talkable.com>
2023-02-28 15:16:12 +0200

    remove unsupported value

--- gdrive_sync.sh
+++ gdrive_sync.sh
@@ -1,3 +1,5 @@
 #!/bin/sh -e
-rclone copy -vv models/ drive:models --drive-upload-cutoff 1000T --create-empty-src-dirs
-rclone copy -vv drive:models models/ --drive-upload-cutoff 1000T --create-empty-src-dirs
+
+#rclone copy -vv models/ drive:models --drive-upload-cutoff 1000T --create-empty-src-dirs
+#rclone copy -vv drive:models models/ --drive-upload-cutoff 1000T --create-empty-src-dirs
+rclone mkdir drive:$(hostname | cut -d. -f1)
