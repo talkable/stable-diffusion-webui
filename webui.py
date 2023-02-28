c85766086b962093d1ebdbce4104ff00c2342318
sergii.kostiuk <sergii.kostiuk@talkable.com>
2023-02-28 15:16:12 +0200

    remove unsupported value

--- webui.py
+++ webui.py
@@ -260,7 +260,6 @@ def webui():
             server_port=cmd_opts.port,
             ssl_keyfile=cmd_opts.tls_keyfile,
             ssl_certfile=cmd_opts.tls_certfile,
-            description="Talkable Art Terminal",
             favicon_path="favicon.ico",
             debug=cmd_opts.gradio_debug,
             auth=[tuple(cred.split(':')) for cred in gradio_auth_creds] if gradio_auth_creds else None,
