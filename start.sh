7630d879a76b78e0b7cc8c4060cb03c70fe3999b
sergii.kostiuk <sergii.kostiuk@talkable.com>
2023-02-28 15:16:12 +0200

    remove unsupported value

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
+#!/bin/sh -e
+
 cd /home/ubuntu/stable-diffusion-webui
-git fetch
-git reset --hard origin/master
+#git fetch
+#git reset --hard origin/master
 screen -dmS main -t tab0 /home/ubuntu/stable-diffusion-webui/webui.sh --listen --enable-insecure-extension-access --xformers
-screen -S main -X screen -t tab1 /home/ubuntu/stable-diffusion-webui/gdrive_sync.sh
+screen -S main -X screen -t tab1 /home/ubuntu/stable-diffusion-webui/s3_sync.sh
